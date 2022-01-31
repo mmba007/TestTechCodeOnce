9bd824870d1d91c227338710a52b671cde39dde9
mmba007 <mohamedmahmoud.benazzzouna@gmail.com>
2022-01-31 17:55:17 +0100

    fixing cors

--- testTech/src/main/java/fr/codeonce/testTech/exception/RecordNotFoundException.java
+++ testTech/src/main/java/fr/codeonce/testTech/exception/RecordNotFoundException.java
@@ -1,9 +1,5 @@
 package fr.codeonce.testTech.exception;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
- 
-
 public class RecordNotFoundException extends RuntimeException 
 {
     public RecordNotFoundException(String exception) {
