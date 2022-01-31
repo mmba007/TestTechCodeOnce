cc7096f093766f542dd6d54e27c536388fa94e0b
mmba007 <mohamedmahmoud.benazzzouna@gmail.com>
2022-01-31 17:07:49 +0100

    fixing cors

--- testTech/src/main/java/fr/codeonce/testTech/controller/ProductsController.java
+++ testTech/src/main/java/fr/codeonce/testTech/controller/ProductsController.java
@@ -25,7 +25,7 @@ import fr.codeonce.testTech.service.ProductService;
 
 @RestController
 @RequestMapping("/api")
-@CrossOrigin("localhost:80")
+@CrossOrigin("http://front:80")
 public class ProductsController {
 
 	@Autowired
